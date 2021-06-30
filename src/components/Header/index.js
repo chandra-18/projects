2149994979a43366bcc333b754bc4c37ab037eb5
chandra-18 <chandrakanthvallamreddy07@gmail.com>
2021-06-30 12:47:15 +0000

    portfolio

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,28 +3,34 @@ import './index.css'
 const Header = () => (
   <nav className="nav-header">
     <div className="nav-content">
-      <p className="website-logo">RA</p>
+      <p className="website-logo">ck</p>
       <ul className="nav-menu">
         <li>
-          <img
-            className="social-network-img"
-            src="https://image.flaticon.com/icons/png/512/174/174857.png"
-            alt="Linked In"
-          />
+          <a href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile">
+            <img
+              className="social-network-img"
+              src="https://image.flaticon.com/icons/png/512/174/174857.png"
+              alt="Linked In"
+            />
+          </a>
         </li>
         <li>
-          <img
-            className="social-network-img"
-            src="https://image.flaticon.com/icons/png/512/25/25231.png"
-            alt="Git Hub"
-          />
+          <a href="https://github.com/">
+            <img
+              className="social-network-img"
+              src="https://image.flaticon.com/icons/png/512/25/25231.png"
+              alt="Git Hub"
+            />
+          </a>
         </li>
         <li>
-          <img
-            className="social-network-img"
-            src="https://res.cloudinary.com/dmwrugc6z/image/upload/v1621681890/Background_4x_1_bzyjew.png"
-            alt="Twitter"
-          />
+          <a href="https://twitter.com/home">
+            <img
+              className="social-network-img"
+              src="https://res.cloudinary.com/dmwrugc6z/image/upload/v1621681890/Background_4x_1_bzyjew.png"
+              alt="Twitter"
+            />
+          </a>
         </li>
       </ul>
     </div>
